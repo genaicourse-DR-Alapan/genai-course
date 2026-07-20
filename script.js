7c9a7a1cdb200279abc0e72eeaedcf7fa90be1e6
genaicourse-DR-Alapan <genaicourse984@gmail.com>
2026-07-20 19:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// Progress Bar
+
+window.onscroll = function () {
+
+let winScroll =
+document.body.scrollTop ||
+document.documentElement.scrollTop;
+
+let height =
+document.documentElement.scrollHeight -
+document.documentElement.clientHeight;
+
+let scrolled = (winScroll / height) * 100;
+
+document.getElementById("progressBar").style.width =
+scrolled + "%";
+
+// Scroll button
+
+const btn =
+document.getElementById("topBtn");
+
+if(winScroll>400)
+btn.style.display="block";
+else
+btn.style.display="none";
+
+};
+
+// Scroll Top
+
+document.getElementById("topBtn")
+.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
+
+// Fade Animation
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity=1;
+
+entry.target.style.transform="translateY(0px)";
+
+}
+
+});
+
+});
+
+document.querySelectorAll(
+
+'.module-card,.about-card,.evaluation-card,.book-card,.timeline-content,.milestone-card'
+
+).forEach(el=>{
+
+el.style.opacity=0;
+
+el.style.transform="translateY(60px)";
+
+el.style.transition=".8s";
+
+observer.observe(el);
+
+});
